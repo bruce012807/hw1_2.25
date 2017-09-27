df3f7793e02cc2531f6c73fb7ffc19c245e6eb46
bruce012807 <bruce012807@gmail.com>
2017-09-28 03:03:54 +0800

    fuck you

---
+++ hw/hw1/hw1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("    j          j\n");
+	printf("   j           j\n");
+	printf("    jjjjjjjjjjjj\n");
+	printf("               j\n");
+	printf("               j\n");
+	printf("                \n");
+	printf("   iiiiiiiiii ii\n");
+	printf("                \n");
+	printf("   mmmmmmmmmmmmm\n");
+	printf("             m\n");
+	printf("              m\n");
+	printf("             m\n");
+	printf("   mmmmmmmmm\n");
+	printf("             m\n");
+	printf("              m\n");
+	printf("   mmmmmmmmmmm\n");
+	system("pause");
+	return 0;
+}
